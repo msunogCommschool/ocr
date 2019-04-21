928b3f8b5a977a8c1086acbcddff2ac3187fdb40
msunogCommschool <46491330+msunogCommschool@users.noreply.github.com>
2019-04-21 16:36:22 -0400

    Add files via upload
    Has a functional neural network to identify characters.
    Correctly identified 1 as 1.
    Incorrectly identified 0 as 1.  
    Bias to 1 may come from fewer structures that make it up (which I tried to account for by scaling).

--- ocr.py
+++ ocr.py
@@ -72,7 +72,7 @@ def pixelAtPos(array, x, y):
 	return array[y][x]
 
 
-
+#***This Shouldn't return 0 easily.
 # Returns a value based on how percentage of non-white pixels in 5x5 block
 # Returns a value between 0 and 1
 # Uses sqrt(.04*x) for 0 <= x <= 25
@@ -84,7 +84,9 @@ def evalBlock(array):
 			if (pixel > 0):
 				nonWhiteCount = nonWhiteCount + 1
 
-	return (sqrt(.04 * nonWhiteCount))
+	# Adding .1 t return value
+	# To fix mistake of 0 trumping, obv not a permanent solution.
+	return (sqrt(.04 * nonWhiteCount)) + .1
 
 # Returns a list of the structure section evaluations
 # List[List[Int]] -> List[Int]
@@ -98,33 +100,171 @@ def getStructureSectionEvals(array):
 	return evalList
 
 
+# Returns a list of the structure evaluation dictionaries
+# (The top level)
+# String -> List[Dict[String -> int]]
+def getImageStructureEvals(path):
+	evalList = []
+	for i in range(0, 3):
+		for j in range(0, 3):
+			structure = makeImageSection(path, j, i)
+			evalList.append(makeStructDict(structure))
+
+	return evalList
+
 
 # Creates a dictionary with values for each type of structure for a given image array
+# maybe switch to class
 # array -> dict
-"""
+
 def makeStructDict(array):
 	imgDict = {
-	"horizLine": getHorizLineVal(array)
-	"horizLine": getHorizLineVal(array)
-	"horizLine": getHorizLineVal(array)
-	"horizLine": getHorizLineVal(array)
-	"horizLine": getHorizLineVal(array) 
+		"horizLine": horizLineVal(array),
+		"vertLine": vertLineVal(array),
+		"leftDiag": leftDiagLineVal(array),
+		"rightDiag": rightDiagLineVal(array),
+		"leftUp": leftUpCurveVal(array),
+		"leftDown": leftDownCurveVal(array),
+		"rightUp": rightUpCurveVal(array),
+		"rightDown": rightDownCurveVal(array) 
 	}
 
 	return imgDict
-"""
+
+
 # The following functions should all be
 # List[List[Int]] -> Int (between 0 and 1)
+# Make these return nth rooth
 
-
-def getHorizLineVal(array):
+def horizLineVal(array):
 	evalList = getStructureSectionEvals(array)
 
-	topLine = evalList[0] * evalList[1] * evalList[2]
-	midLine = evalList[3] * evalList[4] * evalList[5]
-	botLine = evalList[6] * evalList[7] * evalList[8]
+	topLine = (evalList[0] * evalList[1] * evalList[2])**(1.0/3)
+	midLine = (evalList[3] * evalList[4] * evalList[5])**(1.0/3)
+	botLine = (evalList[6] * evalList[7] * evalList[8])**(1.0/3)
 
 	return max(topLine, midLine, botLine)
+
+def vertLineVal(array):
+	evalList = getStructureSectionEvals(array)
+
+	leftLine = (evalList[0] * evalList[3] * evalList[6])**(1.0/3)
+	midLine = (evalList[2] * evalList[5] * evalList[7])**(1.0/3)
+	rightLine = (evalList[3] * evalList[6] * evalList[8])**(1.0/3)
+
+	return max(leftLine, midLine, rightLine)
+
+def leftDiagLineVal(array):
+	evalList = getStructureSectionEvals(array)
+
+	line = (evalList[6] * evalList[4] * evalList[2])**(1.0/3)
+
+	return line
+
+def rightDiagLineVal(array):
+	evalList = getStructureSectionEvals(array)
+
+	line = (evalList[0] * evalList[4] * evalList[8])**(1.0/3)
+
+	return line
+
+def leftUpCurveVal(array):
+	evalList = getStructureSectionEvals(array)
+
+	topLine = (evalList[3] * evalList[4] * evalList[1] * (1 - evalList[0]))**(1.0/4)
+	botLine = (evalList[7] * evalList[8] * evalList[5] * (1 - evalList[0]))**(1.0/4)
+
+	return max(topLine, botLine)
+
+#Can I standardize all of these, 4 of these.
+def leftDownCurveVal(array):
+	evalList = getStructureSectionEvals(array)
+
+	topLine = (evalList[3] * evalList[4] * evalList[7] * (1 - evalList[6]))**(1.0/4)
+	botLine = (evalList[1] * evalList[2] * evalList[5] * (1 - evalList[6]))**(1.0/4)
+
+	return max(topLine, botLine)
+
+def rightUpCurveVal(array):
+	evalList = getStructureSectionEvals(array)
+
+	topLine = (evalList[1] * evalList[4] * evalList[5] * (1 - evalList[2]))**(1.0/4)
+	botLine = (evalList[3] * evalList[6] * evalList[7] * (1 - evalList[2]))**(1.0/4)
+
+	return max(topLine, botLine)
+
+def rightDownCurveVal(array):
+	evalList = getStructureSectionEvals(array)
+
+	topLine = (evalList[3] * evalList[0] * evalList[1] * (1 - evalList[8]))**(1.0/4)
+	botLine = (evalList[7] * evalList[4] * evalList[5] * (1 - evalList[8]))**(1.0/4)
+
+	return max(topLine, botLine)
+
+
+
+# Character Structure Dictionaries
+# pair characters to tuples
+# lists containging tuples contain structure type followed by number of non blank structures
+# (for nth rooting to balance)
+
+charStructDict = {
+	"t": [["horizLine", "vertLine", "horizLine", "", "vertLine", "", "" "vertLine", ""]],
+	"1": [["vertLine", "", "", "vertLine", "", "", "vertLine" "", ""],
+	["", "vertLine", "", "", "vertLine", "", "vertLine" "", ""],
+	["", "", "vertLine", "", "", "vertLine", "" "", "vertLine"]],
+	"0": [["rightDown", "horizLine", "leftDown", "vertLine", "", "vertLine", "rightUp", "horizLine", "leftUp"]]
+}
+
+
+# Given a structure array, returns the probability that it is a given char
+# List[Dict[String -> int]] String -> Int
+def charProbability(dict, char):
+	checkDicts = charStructDict[char]
+
+	max = 0
+	for checkDict in checkDicts:
+		if dictAffinity(dict, checkDict) > max:
+			max = dictAffinity(dict, checkDict)
+	return max
+
+# Returns how strong two dicts match
+# List[Dict[String -> int]] List[String] Int -> Int
+def dictAffinity(dict, matches):
+	affinity = 1
+	mult = 0
+	for i in range(8):
+		if matches[i] != "":
+			mult = mult + 1
+			affinity = affinity * dict[i][matches[i]]
+
+	return affinity ** (1/float(mult))
+
+
+
+
+
+
+
+
+"""
+left up: 3, 4, 1 | 7, 8, 5 (0)
+left down: 3, 4, 7 | 1, 2, 5 (6)
+right up: 1, 4, 5 | 3, 6, 7 (2)
+**right down: 3, 0, 1 | 7, 4, 5 ()
+find a func
+
+# possibly standardiation
+# horiz and vert strings left right, up down.
+def curveVal(array, horiz, vert):
+
+	if horiz == left:
+
+	topLine = (evalList[a] * evalList[b] * evalList[c] * (1 - evalList[d]))**(1.0/4)
+	botLine = (evalList[e] * evalList[f] * evalList[g] * (1 - evalList[d]))**(1.0/4)
+"""
+
+
 
 
 
